495859f2a58807582f9ae8c7eaf855e07b94cf85
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-01-04 19:24:36 +0530

    change origin

--- backend/main.js
+++ backend/main.js
@@ -79,7 +79,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',  // Frontend running on this port
+  origin: 'https://frontend-pearl-two-61.vercel.app/',  // Frontend running on this port
   methods: 'GET, POST, PUT, DELETE',
 }));
 
