d052cdfe3e1f8f779fd9e7e9cf36282746b375ce
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-01-05 04:55:19 +0530

    add core origin

--- backend/main.js
+++ backend/main.js
@@ -1,75 +1,3 @@
-// const express = require("express");
-// const { MongoClient, ObjectId } = require("mongodb");
-// require("dotenv").config();
-// const cors = require("cors");
-
-// const app = express();
-
-// // Middleware
-// app.use(cors({
-//   origin: 'http://localhost:3000',  // Frontend running on this port
-//   methods: 'GET, POST, PUT, DELETE',
-// }));
-
-// app.use(express.json());
-
-// const mongoose = require('mongoose');
-
-// const mongoUrl = process.env.MONGO_URL;
-
-// // try {
-// //   mongoose.connect(mongoUrl, {
-// //     useNewUrlParser: true,
-// //     useUnifiedTopology: true,
-// //     serverSelectionTimeoutMS: 50000,  // Increase timeout duration
-// //     socketTimeoutMS: 45000
-// //   }).then(() => {
-// //       console.log('Connected to MongoDB');
-// //   }).catch((err) => {
-// //       console.error('MongoDB connection error:', err);
-// //   });
-  
-// // } catch (error) {
-// //   console.error('Error occurred:', error.message);
-// //   res.status(500).send('Internal Server Error');
-// // }
-
-
-// // MongoDB Connection URI
-// const uri = process.env.MONGO_URL;
-// const client = new MongoClient(uri);
-
-// // Connect MongoDB once at the start
-// let db;
-
-// async function connectDB() {
-//   try {
-//     if (!db) {
-//       await client.connect();
-//       console.log("Connected to MongoDB");
-//       db = client.db("Shoes");  // Select the "Shoes" database
-//     }
-//   } catch (err) {
-//     console.error("Error connecting to MongoDB:", err);
-//     process.exit(1);  // Exit the process if MongoDB connection fails
-//   }
-// }
-
-// connectDB();
-
-// // Route to fetch all products
-// app.get("/api/products", async (req, res) => {
-//   try {
-//     const products = await db.collection("products_collection").find({}).toArray();
-//     // console.log(products);
-    
-//     res.status(200).json(products);
-//   } catch (error) {
-//     console.error("Error fetching products:", error);
-//     res.status(500).json({ message: "Error fetching products" });
-//   }
-// });
-
 const express = require("express");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
@@ -78,23 +6,6 @@ const cors = require("cors");
 const app = express();
 
 // Middleware
-// const allowedOrigins = [
-//   'https://frontend-pearl-two-61.vercel.app',
-//   'https://shoes-shine-th6dq.vercel.app',
-//   'http://localhost:5173/'
-// ];
-
-// app.use(cors({
-//   origin: (origin, callback) => {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//           callback(null, true);
-//       } else {
-//           callback(new Error('Not allowed by CORS'));
-//       }
-//   },
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
 app.use(cors())
 
 // MongoDB connection variables
