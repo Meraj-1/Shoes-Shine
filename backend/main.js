a1861bf8332faa15f7ce4054dabf5ca39d816802
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-01-05 05:53:21 +0530

    added core

--- backend/main.js
+++ backend/main.js
@@ -6,7 +6,12 @@ const cors = require("cors");
 const app = express();
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production' ? 'https://shoes-shine-xwrd.vercel.app' : '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+};
+app.use(cors(corsOptions));
 
 // MongoDB connection variables
 const uri = process.env.MONGO_URL;
@@ -15,7 +20,6 @@ let cachedDb = null;
 // Function to get the database connection
 async function connectDB() {
   if (cachedDb) {
-    // If the database connection is cached, reuse it
     return cachedDb;
   }
 
@@ -34,10 +38,10 @@ async function connectDB() {
   }
 }
 
-// Route to fetch all products
+// Routes
 app.get("/api/products", async (req, res) => {
   try {
-    const db = await connectDB(); // Ensure the database is connected
+    const db = await connectDB();
     const products = await db.collection("products_collection").find({}).toArray();
     res.status(200).json(products);
   } catch (error) {
@@ -46,7 +50,6 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
-// Route to fetch a single product by ID
 app.get("/api/products/:id", async (req, res) => {
   try {
     const productId = req.params.id;
@@ -56,18 +59,13 @@ app.get("/api/products/:id", async (req, res) => {
       return res.status(400).json({ message: "Invalid product ID" });
     }
 
-    // Ensure db is initialized before attempting to query the collection
-    const db = await connectDB(); // Make sure to get the connected db
-
-    // Attempt to find the product in the database
+    const db = await connectDB();
     const product = await db.collection("products_collection").findOne({ _id: new ObjectId(productId) });
 
-    // If product not found, return a 404 error
     if (!product) {
       return res.status(404).json({ message: "Product not found" });
     }
 
-    // If product found, return the product data
     res.json(product);
   } catch (error) {
     console.error("Error fetching product:", error);
@@ -75,7 +73,6 @@ app.get("/api/products/:id", async (req, res) => {
   }
 });
 
-// Default route
 app.get('/', (req, res) => {
   res.send('Welcome to the backend!');
 });
