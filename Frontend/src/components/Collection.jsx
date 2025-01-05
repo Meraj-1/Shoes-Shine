9860c15eca3f966102be344d6e32f75282ec058a
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-01-05 22:19:22 +0530

    change url

--- Frontend/src/components/Collection.jsx
+++ Frontend/src/components/Collection.jsx
@@ -15,7 +15,7 @@ const Collection = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get('https://shoes-shine.vercel.app/api/product');
+        const response = await axios.get('https://shoes-shine.vercel.app/api/products');
         setProducts(response.data);
         setFilteredProducts(response.data);
         setLoading(false);
