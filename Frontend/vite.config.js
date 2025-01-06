cda6369221d3fcfee184358237c623a94c856fe1
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-01-06 20:47:57 +0530

    edit vercel.json

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        // Suppress "Module level directives cause errors" warnings
-        if (warning.message.includes('"use client"')) return;
-        warn(warning);
-      },
-    },
-  },
 });
